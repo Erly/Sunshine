f86cc537102a6487f2322f04b4fda29b34c4f42f
Erlantz Oniga <erlantz8@gmail.com>
2015-06-03 22:51:09 +0200

    Loaded the list with fake data. End of Lesson 1.

--- app/src/main/java/com/erlantzoniga/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/erlantzoniga/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +22,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> forecastEntries = new ArrayList<>(14);
+        forecastEntries.add(0, "Today - Sunny - 88/63");
+        forecastEntries.add(1, "Tomorrow - Foggy - 70/46");
+        forecastEntries.add(2, "Weds - Cloudy - 72/63");
+        forecastEntries.add(3, "Thurs - Rainy - 64/51");
+        forecastEntries.add(4, "Fri - Foggy - 70/46");
+        forecastEntries.add(5, "Sat - Sunny - 76/68");
+        forecastEntries.add(6, "Sun - Sunny - 88/63");
+        forecastEntries.add(7, "Mon - Cloudy - 73/64");
+        forecastEntries.add(8, "Tue - Sunny - 89/68");
+        forecastEntries.add(9, "Weds - Cloudy - 75/63");
+        forecastEntries.add(10, "Thurs - Rainy - 70/60");
+        forecastEntries.add(11, "Fri - Rainy - 68/56");
+        forecastEntries.add(12, "Sat - Foggy - 73/66");
+        forecastEntries.add(13, "Sun - Sunny - 88/63");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastEntries);
+
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(arrayAdapter);
+        return rootView;
     }
 }
